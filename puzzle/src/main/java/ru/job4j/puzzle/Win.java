8948f30f1e150bf101349debaf9e6d1e14f908af
Aktsent <accent43@yandex.ru>
2021-10-05 15:52:32 +0300

    Win

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,10 +2,10 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-                if (board[i][i] == 0 && Win.horizont(board, i) || Win.vertical(board, i)) {
-                    rsl = false;
+                if (board[i][i] == 1 && horizont(board, i) || vertical(board, i)) {
+                    rsl = true;
                     break;
             }
         }
@@ -13,10 +13,10 @@ public class Win {
     }
 
     public static boolean horizont(int[][] board, int row) {
-        boolean rsl = false;
+        boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
-                if (board[row][i] != 0) {
-                    rsl = true;
+                if (board[row][i] != 1) {
+                    rsl = false;
                     break;
                 }
         }
@@ -24,10 +24,10 @@ public class Win {
     }
 
     public static boolean vertical(int[][] board, int column) {
-        boolean rsl = false;
+        boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
-                if (board[i][column] != 0) {
-                    rsl = true;
+                if (board[i][column] != 1) {
+                    rsl = false;
                     break;
                 }
         }
