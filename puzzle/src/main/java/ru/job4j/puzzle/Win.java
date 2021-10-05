b20f113411b6d6f628f01e60e0d43cf95901e4ab
Aktsent <accent43@yandex.ru>
2021-10-05 12:53:48 +0300

    Win

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,34 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                Win.horizont(board);
+                Win.vertical(board);
+            }
+        }
         return rsl;
+    }
+
+    public static boolean horizont(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean vertical(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[j][i] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
