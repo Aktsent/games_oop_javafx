ae7e50a9399ea57277e8ed5c215e3e15756d73d8
Aktsent <accent43@yandex.ru>
2022-03-16 11:32:51 +0300

    BishopBlack

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -23,10 +23,10 @@ public class BishopBlack implements Figure {
                 String.format("Could not way by diagonal from %s to %s", position, dest)
             );
         }
-        int size = 5;
+        int size = Math.abs(dest.getY() - position.getY());
         Cell[] steps = new Cell[size];
-        int deltaX = 1;
-        int deltaY = 1;
+        int deltaX = dest.getX() - position.getX() > 0 ? 1 : -1;
+        int deltaY = dest.getY() - position.getY() > 0 ? 1 : -1;
         int x = position().getX();
         int y = position().getY();
         for (int index = 0; index < size; index++) {
@@ -39,10 +39,7 @@ public class BishopBlack implements Figure {
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        if (Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY())) {
-            return true;
-        }
-        return false;
+        return Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY());
     }
 
     @Override
