c1eab2d9961501aa6a567515ec2bb6eccbf73482
Aktsent <accent43@yandex.ru>
2022-03-17 16:04:54 +0300

    BishopBlackTest

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,20 +1,29 @@
 package ru.job4j.chess.firuges.black;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class BishopBlackTest {
 
     @Test
     public void testPosition() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.B8);
-        bishopBlack.position();
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.position().equals(Cell.C1);
     }
 
     @Test
     public void testCopy() {
         BishopBlack bishopBlack = new BishopBlack(Cell.B8);
-        bishopBlack.copy(Cell.B8);
+        bishopBlack.copy(Cell.B2);
+    }
+
+    @Test
+    public void testWay() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        bishopBlack.way(Cell.G5);
+        bishopBlack.isDiagonal(Cell.C1, Cell.G5);
     }
 }
