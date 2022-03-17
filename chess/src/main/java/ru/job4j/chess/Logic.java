b4f6f85564e82686118d3622ee437c7ef0e7f231
Aktsent <accent43@yandex.ru>
2022-03-17 10:04:02 +0300

    Logic

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -21,10 +21,11 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
-        for (int i = 0; i < figures.length; i++) {
-            Figure figure = figures[i];
-            if (figure.position().equals(steps[i])) {
-                throw new OccupiedCellException();
+        for (Cell step : steps) {
+            for (Figure figure : figures) {
+                if (figure.position().equals(step)) {
+                    throw new OccupiedCellException();
+                }
             }
         }
         return true;
